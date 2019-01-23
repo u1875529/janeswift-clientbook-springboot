e42dd280d5a515560434c927997e759bb2772783
u18775529 <43642672+u18775529@users.noreply.github.com>
2019-01-23 22:14:10 +0000

    Downloaded Thymeleaf libraries Created controller pkg Added Java Class ClientBookController

--- src/main/java/com/janeswift/clientbook/controller/ClientBookController.java
+++ src/main/java/com/janeswift/clientbook/controller/ClientBookController.java
@@ -1,4 +1,14 @@
 package com.janeswift.clientbook.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@ResponseBody
+@RestController
 public class ClientBookController {
+    @GetMapping ("/")
+    public String testMapping (){
+        return "Hello, World";
+    }
 }
