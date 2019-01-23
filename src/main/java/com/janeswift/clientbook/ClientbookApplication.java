8bdb8a052dbfa0a010ad3807c0916a644104941a
u18775529 <43642672+u18775529@users.noreply.github.com>
2019-01-23 21:01:12 +0000

    Initial Commit

---
+++ src/main/java/com/janeswift/clientbook/ClientbookApplication.java
@@ -0,0 +1,14 @@
+package com.janeswift.clientbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientbookApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/janeswift/clientbook/ClientbookApplicationTests.java
@@ -0,0 +1,17 @@
+package com.janeswift.clientbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientbookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
